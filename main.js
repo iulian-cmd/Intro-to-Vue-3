b6df99aa6d2ee7b4ff0dcff52c92c846dbcf1f7e
iulian-cmd <flysteelbird@gmail.com>
2021-11-06 17:29:38 +0100

    test onclick

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
+            product: 'Bike',
+            image: './assets/images/001black/black001.JPG',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['Brakes', 'Frame', 'Saddle'],
+            variants: [{
+                    variantId: 01,
+                    variantVue: 'side',
+                    variantImage: './assets/images/001black/black001.JPG'
+                },
+                {
+                    variantId: 02,
+                    variantVue: 'third',
+                    variantImage: './assets/images/001black/black002.JPG'
+                },
+                {
+                    variantId: 03,
+                    variantVue: 'front',
+                    variantImage: './assets/images/001black/black003.JPG'
+                }
+            ]
+        }
+    },
+    methods: {
+        clickMethod() {
+            image = "variant.variantImage" //add code that you wish to happen on click
         }
     }
-})
+});
